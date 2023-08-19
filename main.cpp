4340e3ca6ad43da8c383e2a952a64defb43078ec
kyberdrb <kyberdrb@gmail.com>
2023-08-19 12:48:53 +0200

    MicroTetris animation...
    ...OVERLINE as a floor for gaming field - https://stackoverflow.com/questions/61690230/chinese-character-too-large-for-enclosing-character-literal-type/61690259#61690259
        time between rendering frames - http://underthepressure.net/how-to-sleep-in-c/
        clear console between each frame - https://www.tutorialspoint.com/how-can-i-clear-console-using-cplusplus
        OS specific command to clear terminal - https://stackoverflow.com/questions/6649936/c-compiling-on-windows-and-linux-ifdef-switch/6649992#6649992

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 int main() {
-    std::cout << "Hello, World!\n" << std::flush;
+    const char16_t OVERLINE = u'‾';
+
+    #ifdef _WIN32
+        std::system("cls");
+    #else
+        std::system("clear");
+    #endif
+
+    std::cout << "ConstructionSite (stavenisko - hracia plocha)" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "|   |" << std::endl;
+    std::cout << "|   |" << std::endl;
+    std::cout << "‾‾‾‾‾" << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    #ifdef _WIN32
+        std::system("cls");
+    #else
+        std::system("clear");
+    #endif
+
+    std::cout << "Brick (one part of a Tetromino - tehlička)" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "| # |" << std::endl;
+    std::cout << "|   |" << std::endl;
+    std::cout << "‾‾‾‾‾" << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    #ifdef _WIN32
+        std::system("cls");
+    #else
+        std::system("clear");
+    #endif
+
+    std::cout << "Brick has fallen to the bottom - this brick stops moving and next brick starts falling" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "|   |" << std::endl;
+    std::cout << "| # |" << std::endl;
+    std::cout << "‾‾‾‾‾" << std::endl;
+
+    std::cin.get();
+
     return 0;
 }
-
